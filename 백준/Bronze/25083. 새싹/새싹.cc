4eefd612c80d65886328fed84e04b52887da353f
swatper <67682393+swatper@users.noreply.github.com>
2024-03-31 00:51:58 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string Sprout[6] = {"         ,r'\"7"
+						,"r`-_   ,'  ,/"
+						," \\. \". L_r'"
+						,"   `~\\/"
+						,"      |"
+						,"      |"};
+	for (int i = 0; i < 6; i++) {
+		cout << Sprout[i] << "\n";
+	}
+	return 0;
+}
