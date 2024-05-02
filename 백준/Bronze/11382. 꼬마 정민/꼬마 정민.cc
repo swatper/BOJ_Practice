777d3ed82ebe2604c47d9809f751025e86a7ace0
swatper <67682393+swatper@users.noreply.github.com>
2024-05-02 13:39:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	long long A, B, C;
+	cin >> A >> B >> C;
+	cout << A + B + C << "\n";
+	return 0;
+}
