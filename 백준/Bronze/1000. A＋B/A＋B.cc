c3341338fe6d091dc752b2ad262bf901f7fe59eb
swatper <67682393+swatper@users.noreply.github.com>
2024-05-12 12:17:31 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int NumA, NumB;
+    cin >> NumA >> NumB;
+    cout << NumA + NumB << "\n";
+    return 0;
+}
