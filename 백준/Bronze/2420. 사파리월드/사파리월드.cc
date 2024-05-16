256c09e0cef77c652c5efc1359714a371bab639e
swatper <67682393+swatper@users.noreply.github.com>
2024-05-16 14:10:10 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    long long Num1, Num2;
+    cin >> Num1 >> Num2;
+    cout << abs(Num1 - Num2) << "\n";
+    return 0;
+}
