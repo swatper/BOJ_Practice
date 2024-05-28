d9d2db397cddfcc51bc6d203d5d24c5ae7799ac4
swatper <67682393+swatper@users.noreply.github.com>
2024-05-28 22:38:03 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << "\n";
+    return 0;
+}
