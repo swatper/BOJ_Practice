fb6240d707ba45d7dc5a81cae45cc432b1ada40c
swatper <67682393+swatper@users.noreply.github.com>
2024-06-06 20:15:39 +0900

    [Silver V] Title: 아무래도이문제는A번난이도같다, Time: 0 ms, Memory: 2024 KB -BaekjoonHub
    GIt에 자동 commit이슈로 직접 commit

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도같다/아무래도이문제는A번난이도같다.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int Tc;
+	cin >> Tc;
+	for (int i = 0; i < Tc; i++) {
+		int NumA, NumB;
+		cin >> NumA >> NumB;
+		//NumA를 a1*a2*a3..로 인수분해 하였을 때
+		//1과 -1을 적절히 사용하여 표현하면
+		//얼마든지 NumB를 만들 수 있음
+		//5: 5 * (-1) * (-1) *(-1) * (-1) -> 1
+		//5 * (-1) * (-1) *(-1) * (-1) -> 2   
+		//5: 5 * (-1) * (-1)-> 3
+		//5: 5 * (-1) * (-1) * (1) -> 4
+		//5: 5 * (-1) * (-1) * (1)  * (1) -> 5
+		//5: 5 * (1) -> 6
+		//그러므로 모든 숫자를 표현할 수 있음
+		cout << "yes\n";
+	}
+	return 0;
+}
+//Github에 자동 연동이 되지않아 수동으로 연동
