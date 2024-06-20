355063ea3456c8240fd41a13f50e3121a4c47e6f
swatper <67682393+swatper@users.noreply.github.com>
2024-06-20 19:49:29 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cout << ".  .   .\n";
+	cout << "|  | _ | _. _ ._ _  _\n";
+	cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+	return 0;
+}    
