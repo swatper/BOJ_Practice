6106e495ac6bff5996fcde8b2a7b51e489b75785
swatper <67682393+swatper@users.noreply.github.com>
2024-06-28 23:19:55 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int numA, numB;
+	cin >> numA >> numB;
+	cout << numA + numB << "\n";
+	cout << numA - numB << "\n";
+	cout << numA * numB << "\n";
+	cout << numA / numB << "\n";
+	cout << numA % numB << "\n";
+	return 0;
+}    
