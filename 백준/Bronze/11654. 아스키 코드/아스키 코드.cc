02ddf732edbf24e66a27e1bf213696567058ad7c
swatper <67682393+swatper@users.noreply.github.com>
2024-07-18 22:49:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	char cha;
+	cin >> cha;
+	//강제 형 변환
+	cout << (int)cha << "\n";
+	return 0;
+}
