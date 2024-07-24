236e65c0ad41809ca609d2d7d956e4aa4d597e64
swatper <67682393+swatper@users.noreply.github.com>
2024-07-24 21:13:02 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++){
+		cout << i << "\n";
+	}
+	return 0;
+}
