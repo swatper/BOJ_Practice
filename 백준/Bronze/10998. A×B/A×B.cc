67dd2a4b0240de8f707f4383a54a0a1ac2772f94
swatper <67682393+swatper@users.noreply.github.com>
2024-07-30 12:38:14 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);	cout.tie(NULL);
+
+	ios_base::sync_with_stdio(false);
+
+	int A , B;
+
+	cin >> A >> B;
+
+	cout <<   A * B << "\n";
+
+	return 0;
+
+}
