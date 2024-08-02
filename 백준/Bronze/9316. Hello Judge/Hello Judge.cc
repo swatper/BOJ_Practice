75c4700cf73606bef9542e79ec8c6c810e221e04
swatper <67682393+swatper@users.noreply.github.com>
2024-08-02 13:37:14 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int Tc;
+	cin >> Tc;
+	for (int i = 1; i <= Tc; i++)
+	{
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+	return 0;
+}
