6c6049f6d3657db93b21eb7a588b99e8ed5360a7
swatper <67682393+swatper@users.noreply.github.com>
2024-08-15 23:19:20 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cout << "강한친구 대한육군\n";
+	cout << "강한친구 대한육군\n";
+	return 0;
+}
