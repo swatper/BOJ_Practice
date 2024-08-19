537a938e4593d7344e187434b8da9908a20b8de9
swatper <67682393+swatper@users.noreply.github.com>
2024-08-20 00:04:38 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string ID;
+	cin >> ID;
+	cout << ID + "??!\n";
+	return 0;
+}
