401b449ccad810bdc4c79cb57bf0b26df4ffc85d
swatper <67682393+swatper@users.noreply.github.com>
2025-09-24 14:10:42 +0900

    [Silver V] Title: 사탕, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11256. 사탕/사탕.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool Compare(int a, int b);
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int Tc;
+    cin >> Tc;
+    for (int i = 0; i < Tc; i++){
+        int boxes, candies, reqBoxes = 0;
+        cin >> candies >> boxes;
+        //박스 크기를 저장할 배열
+        int* boxList = new int[boxes];
+
+        for (int j = 0; j < boxes; j++){
+            int width, length;
+            cin >> width >> length;
+            boxList[j] = width * length;
+        }
+
+        //내림차순 정렬
+        sort(boxList, boxList + boxes, Compare);
+        for (int j = 0; j < boxes; j++){
+            //박스 사용
+            candies -= boxList[j];
+            reqBoxes = j;
+            if (candies <= 0) {
+                break;
+            }
+        }
+        cout << reqBoxes + 1 << "\n";
+    }
+    return 0;
+}
+
+bool Compare(int a, int b) {
+    return a> b;
+}
