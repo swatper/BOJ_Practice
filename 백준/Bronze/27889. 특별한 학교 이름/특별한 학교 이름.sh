14d657f0e4449f6d476ad01459d9d1e69fc9ad45
swatper <67682393+swatper@users.noreply.github.com>
2025-10-29 17:21:58 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 4 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.sh
@@ -0,0 +1,10 @@
+read a
+if [ "$a" = "NLCS" ]; then 
+    echo "North London Collegiate School"
+elif [ "$a" = "BHA" ]; then 
+    echo "Branksome Hall Asia"
+elif [ "$a" = "KIS" ]; then 
+    echo "Korea International School"
+elif [ "$a" = "SJA" ]; then 
+    echo "St. Johnsbury Academy"
+fi 
