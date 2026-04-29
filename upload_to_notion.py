b0da4efc8a7039bbb3795cf0b35126ed0d54a255
swatper <sh010510@naver.com>
2026-04-29 20:03:05 +0900

    선택적 업데이트 추가

--- upload_to_notion.py
+++ upload_to_notion.py
@@ -23,20 +23,36 @@ headers = {
 
 
 def main():
+    run_mode = os.environ.get('RUN_MODE', 'init')
     base_paths = ["./백준", "./프로그래머스"] 
-    # 현재 디렉토리부터 하위 폴더를 모두 탐색
-    for base_path in base_paths:
-        if not os.path.exists(base_path):
-            print(f"❌ 폴더를 찾을 수 없습니다: {base_path}")
-            return
-        else:
-            if base_path == "./백준":
-                print(f"📂 백준 폴더 탐색 시작: {base_path}")
-                boj.run_baekjoon_process(base_path, NOTION_CONFIG, headers)
+    if run_mode == 'init':
+        print("🚀 초기화 모드로 실행 중...")
+        # 현재 디렉토리부터 하위 폴더를 모두 탐색
+        for base_path in base_paths:
+            if not os.path.exists(base_path):
+                print(f"❌ 폴더를 찾을 수 없습니다: {base_path}")
+                return
+            else:
+                if base_path == "./백준":
+                    print(f"📂 백준 폴더 탐색 시작: {base_path}")
+                    print("🚫 백준 서비스 종료로 인해 스킵")
+                    # boj.run_baekjoon_process(base_path, NOTION_CONFIG, headers)
 
-            elif base_path == "./프로그래머스":
-                print(f"📂 프로그래머스 폴더 탐색 시작: {base_path}")
-                prg.run_programmers_process(base_path, NOTION_CONFIG, headers)
+                elif base_path == "./프로그래머스":
+                    print(f"📂 프로그래머스 폴더 탐색 시작: {base_path}")
+                    prg.run_programmers_process(base_path, NOTION_CONFIG, headers)
+    elif run_mode == 'baekjoon':
+        print("🚀 백준 문제 업데이트 모드로 실행 중...")
+        base_path = base_paths[0]
+        boj.run_baekjoon_process(base_path, NOTION_CONFIG, headers)
+
+    elif run_mode == 'programmers':
+        print("🚀 프로그래머스 문제 업데이트 모드로 실행 중...")
+        base_path = base_paths[1]
+        prg.run_programmers_process(base_path, NOTION_CONFIG, headers)
+    else:
+        print(f"❌ 알 수 없는 실행 모드: {run_mode}.")
+
 
 # GitHub Actions에서 직접 실행할 때는 main() 함수를 호출
 if __name__ == "__main__":
